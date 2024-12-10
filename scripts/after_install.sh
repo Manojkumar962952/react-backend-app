2f9d6e4fc7d6e61ca13b7d5c07366100158c5649
Manojkumar962952 <126284997+Manojkumar962952@users.noreply.github.com>
2024-12-10 14:02:12 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,9 +3,9 @@ set -e
 
 # Navigate to the application directory
 cd /root/backend/react-backend-app
-npm install --force
-pm2 delete all
-npm run serve
+sudo npm install 
+sudo pm2 delete all
+sudo npm run serve
 
 # Install npm dependencies
 #npm install --force
